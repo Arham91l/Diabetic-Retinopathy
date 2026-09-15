2a75a79b44085b10c03d6e49ac691f8cb3beaecc
Shaikh Arham Ahmed <arham07.shaikh@gmail.com>
2026-09-15 11:12:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,8 +32,7 @@ uploaded_file = st.file_uploader("Upload fundus image", type=["jpg", "jpeg", "pn
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
-
+    st.image(image, caption="Uploaded Image", use_container_width=True)
     with st.spinner("Analyzing..."):
         input_tensor = transform(image).unsqueeze(0).to(device)
         with torch.no_grad():
